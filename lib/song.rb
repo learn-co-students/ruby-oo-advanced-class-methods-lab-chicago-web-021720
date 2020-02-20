c9dcf9787c8d65c8a14426af150613f9e201b14b
ellisandrews <ellisandrews1@gmail.com>
2020-02-20 15:15:39 -0600

    Done.

--- lib/song.rb
+++ lib/song.rb
@@ -10,4 +10,71 @@ class Song
     self.class.all << self
   end
 
+  def self.create
+    song = self.new
+    song.save
+    song
+  end
+
+  def self.new_by_name(name)
+    song = self.new
+    song.name = name
+    song
+  end
+
+  def self.create_by_name(name)
+    song = self.new
+    song.name = name
+    song.save
+    song
+  end
+
+  def self.find_by_name(name)
+    self.all.find{ |song| song.name == name}
+  end
+
+  def self.find_or_create_by_name(name)
+    song = self.find_by_name(name)
+    if song
+      return song
+    else
+      self.create_by_name(name)
+    end
+  end
+
+  def self.alphabetical
+    self.all.sort_by { |song| song.name }
+  end
+
+  def self.new_from_filename(filename)
+
+    # Parse the file nane for artist and song name
+    parsed = filename.split(' - ')
+    song_artist = parsed[0]
+    song_name = parsed[1].split('.')[0]  # Strip off '.mp3'
+
+    # Make the new song
+    song = self.new
+    song.name = song_name
+    song.artist_name = song_artist
+    song
+  end
+
+  def self.create_from_filename(filename)
+    # Parse the file nane for artist and song name
+    parsed = filename.split(' - ')
+    song_artist = parsed[0]
+    song_name = parsed[1].split('.')[0]  # Strip off '.mp3'
+
+    # Create the new song
+    song = self.create_by_name(song_name)
+    song.artist_name = song_artist
+    song
+  end
+
+  def self.destroy_all
+    self.all.clear
+  end
+
 end
+
